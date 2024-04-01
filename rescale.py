fddad6dda2e63bcfc7f00ee93825cc1ddcea856b
NaraineSurya <narayanansuryatcr@gmail.com>
2024-04-01 15:54:46 +0530

    rescaling videos

--- rescale.py
+++ rescale.py
@@ -1,10 +1,14 @@
 import cv2
 
-img = cv2.imread('img/dog.webp')
+# img = cv2.imread('img/dog.webp')
 
-cv2.imshow('Image',img)
+# cv2.imshow('Image',img)
 
 # cv2.waitKey(0)
+
+# resized_img = rescaleFrame(img)
+
+# cv2.imshow('Resized', resized_img)
 
 def rescaleFrame(frame, scale=0.75):
     width = int(frame.shape[1] * scale)
@@ -12,8 +16,20 @@ def rescaleFrame(frame, scale=0.75):
 
     return cv2.resize(frame, (width,height), interpolation=cv2.INTER_AREA)
 
-resized_img = rescaleFrame(img)
+capture = cv2.VideoCapture('vid/recvid.mp4')
 
-cv2.imshow('Resized', resized_img)
+while True:
+
+    isTrue, frames = capture.read()
+
+    rescale_Frame = rescaleFrame(frames, 0.4)
+    cv2.imshow('Video', frames)
+    cv2.imshow('rescale_video', rescale_Frame)
+
+    if cv2.waitKey(20) & 0xFF==ord('b'):
+        break
+
+capture.release
+cv2.destroyAllWindows()
 
 cv2.waitKey(0)
