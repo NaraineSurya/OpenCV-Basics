74504572af23865eb6e3451c65e0ec426047fb67
NaraineSurya <narayanansuryatcr@gmail.com>
2024-04-01 16:52:31 +0530

    edge detection

--- func.py
+++ func.py
@@ -7,6 +7,9 @@ new_img  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # cv2.imshow('new_img', new_img)
 
 blur_img = cv2.GaussianBlur(img, (9,9), 0)
-cv2.imshow('blurred_img', blur_img)
+# cv2.imshow('blurred_img', blur_img)
+
+canny = cv2.Canny(img, 100,200)
+cv2.imshow('edges', canny)
 
 cv2.waitKey(0)
