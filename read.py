1541e3df7be1314260f69e296400a4ff78107c2b
NaraineSurya <narayanansuryatcr@gmail.com>
2024-04-01 15:20:27 +0530

    reading simple video

--- read.py
+++ read.py
@@ -1,9 +1,22 @@
 import cv2
 
-img = cv2.imread('img/dog.webp')
+# img = cv2.imread('img/dog.webp')
 
-print(img)
+# print(img)
 
-cv2.imshow('dog',img)
+# cv2.imshow('dog',img)
 
-cv2.waitKey(0)
+# cv2.waitKey(0)
+
+capture = cv2.VideoCapture('vid/recvid.mp4')
+
+while True:
+
+    isTrue, frames = capture.read()
+    cv2.imshow('Video', frames)
+
+    if cv2.waitKey(20) & 0xFF==ord('b'):
+        break
+
+capture.release
+cv2.destroyAllWindows()
